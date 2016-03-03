57015204540497a0b6b5cac3bdcf4fdd216e4842
Wei Liu <liuweiky@qq.com>
2016-03-03 18:42:51 +0800

    调整url_for

--- app/blueprint/blog/views.py
+++ app/blueprint/blog/views.py
@@ -8,8 +8,16 @@
 """
 
 from . import blog
+from flask.ext.security import login_required
+from flask import render_template
 
-@blog.route('/<userid>', methods=['GET', 'POST'])
+@blog.route('/user/<userid>', methods=['GET'])
 def user(userid):
     return '<h1>Blog!</h1>'
+
+
+@login_required
+@blog.route('/post', methods=['GET', 'POST'])
+def post():
+    return  render_template('blog_post.html')
 
